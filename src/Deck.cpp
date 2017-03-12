b62c5064846c2c96738377df8b3941718287d677
yonatanbitton <yonatanbitton1@gmail.com>
2017-03-12 10:13:20 +0200

    Update Deck.cpp

--- src/Deck.cpp
+++ src/Deck.cpp
@@ -126,7 +126,6 @@ void Deck::initDeck(string input){
 	    string shape_str=token.substr(token.size()-1,1);
 	    Card* c1=initCard(value_str, shape_str);
 	    Deck::addCard(c1);
-	    // delete (c1);
 	    s.erase(0, pos + delimiter.length());
 	}
 	if(!(s.empty())){
@@ -134,100 +133,16 @@ void Deck::initDeck(string input){
 		string shape_str=s.substr(s.size()-1,1);
 		Card* c1=initCard(value_str, shape_str);
 		Deck::addCard(c1);
-                // delete (c1);
-//		cout << s << std::endl;
 	}
 }
 
 void Deck::drawKcards(int k, vector<Card*>& asking){ // This function takes K cards from myDeck, and moves it the the asking vector
-//	for (unsigned j=0; j<asking.size(); j++)
-//		cout << "The asking[j] for j=" << j << " is " << asking[j]->toString() << endl;
-//	cout << k << " is the K " << endl;
 	if (k>0){
 		for(int i=0; i<k; i++){
 			if (DeckVec.size() >0){
-//				cout << "BEFORE " << toString() << endl;
 				Card* card=fetchCard();
-//				cout << "AFTER " << toString() << endl;
-//				cout << "The fetchCard is " << card->toString() << endl;
 				asking.push_back(card);
-//				cout << asking.at(asking.size()-1)->toString() << " is the toString of the Card Just entered " << endl;
 			}
 			}
 		}
-//	for (unsigned j=0; j<asking.size(); j++)
-//		cout << "The asking[j] for j=" << j << " is " << asking[j]->toString() << endl;
 }
-
-//void Hand::drawKcards(int k, Deck deck){
-//	if(this->HandVec.size()>0){
-//		if (k!=0){
-//			for(unsigned int i=0; i<k; i++){
-//				if(deck.size()>0){
-//					Card* toInsert=deck.fetchCard();
-//					this.addCard(*toInsert);
-//				}
-//			}
-//		}
-//	}
-//
-//}
-
-
-
-//int main(int argc, char **argv){
-//	string s="KC QH 3D AH JH 2C 3S KS AS JS 3C KH AD QC JD QS 3H KD AC JC 2D 2H 2S QD ";
-//	Card *c1 = new FigureCard(Heart , Jack);
-//	Card *c2=new NumericCard( Club , 8);
-//	Card *c3=new FigureCard(Spade , Ace);
-//	Card *def_c4=new FigureCard();
-//	Card *def_c5=new NumericCard();
-//	vector <Card*> v1;
-//	// v1.push_back(c1);
-//	// cout << "IN THE MAIN to String " << c1->toString() << endl;
-//	//	v1.push_back(c2);
-//	//	v1.push_back(c3);
-//	//	v1.push_back(def_c4);
-//	//	v1.push_back(def_c5);
-//	Deck d1=Deck(v1);
-//	d1.initDeck(s);
-//return 0;
-//}
-//}
-//
-//int main()
-//{
-//Card *c1 = new FigureCard(Heart , Jack);
-//Card *c2=new NumericCard( Club , 8);
-//Card *c3=new FigureCard(Spade , Ace);
-//Card *def_c4=new FigureCard();
-//Card *def_c5=new NumericCard();
-//
-//
-//std::vector<Card*> myvector;
-//
-// // set some values to my wonderfull vector
-//myvector.push_back(c1);
-//myvector.push_back(c2);
-//myvector.push_back(c3);
-//myvector.push_back(def_c4);
-//myvector.push_back(def_c5);
-// // erase the 1st element
-//// myvector.erase (myvector.begin());
-//cout << "myvector contains: ";
-// for (unsigned i=0; i<myvector.size(); ++i)
-//   std::cout << ' ' << myvector[i]->toString() << " ";
-// std::cout << '\n';
-//
-// Deck d1=Deck(myvector);
-//cout << d1.toString() << " " << endl;
-//Card* tmpCard = d1.fetchCard();
-//cout << "The first card is " << tmpCard->toString() << endl;
-//cout << "And now ! " << d1.toString() << " " << endl;
-//
-//cout << "num of cards is:" << d1.getNumberOfCards() << " " << endl ;
-//
-//
-//
-//return 0;
-//}
