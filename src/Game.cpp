6705689186b447832756be30bd9e318ee33d1af8
yonatanbitton <yonatanbitton1@gmail.com>
2017-03-12 10:14:57 +0200

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -30,12 +30,7 @@ vector <string>* Game::readFile(char* configurationFile) {
 	fstream myFile;
 	myFile.open(configurationFile);
 	vector<string>* lines = new vector <string>();
-	//for(unsigned int i=0;i<(*lines).size();i++)
-	//	cout<<(*(*lines)[i])<< endl;
 	string currentLine;
-//	getline(myFile, currentLine);
-//	cout << "Before the while , current line is " << currentLine << endl;
-
 	while (getline(myFile, currentLine)){
 		if ((!currentLine.size()==0) && (currentLine.at(0) !='#') && (currentLine.at(0) !=' ') ){
 			lines->push_back(currentLine);
@@ -49,7 +44,6 @@ vector <string>* Game::readFile(char* configurationFile) {
 Game::Game (const Game& otherGame): players(), winners(), deck(), initState(), verbal(otherGame.getVerbal()), turnsCounter(otherGame.getTurnsCounter()), N(otherGame.getN()), finished(otherGame.getFinished()){
 	for(unsigned int i=0; i<otherGame.getPlayers().size();i++){
 		Player* newPlayer = otherGame.getPlayers()[i]->makePlayer();
-	//	cout << "The " << i << " Player to push is " << newPlayer->toString() << endl;
 		players.push_back(newPlayer);
 	}
 	deck=otherGame.deck; // Uses the copy assignment operator of Deck
@@ -62,24 +56,19 @@ Game::Game (const Game& otherGame): players(), winners(), deck(), initState(), v
 
 // Copy assignment operator
 Game& Game::operator=(const Game& otherGame){
-	//cout << "Im in the operator=" << endl;
 	if (this==&otherGame){ // Maybe if (&*this==&otherGame) ?
 		return *this;
 	}
-	//cout << "Im not myself" << endl;
 	clean();
-	//cout << "Im clean " << endl;
 	this->verbal=otherGame.getVerbal();
 	this->turnsCounter=otherGame.getTurnsCounter();
 	this->N=otherGame.getN();
 	this->deck=otherGame.deck; // Uses the copy assignment operator of Deck
 	this->finished=otherGame.getFinished();
-	//cout << "I have a lot of new fields. Verbal " << verbal << " Turns counter is " << turnsCounter << " N is " << N << " finished is " << finished << " and most imporently the deck is " << deck.toString() << endl;
 
 	for(unsigned int i=0; i<otherGame.getPlayers().size();i++){
 		Player* newPlayer = otherGame.getPlayers()[i]->makePlayer(); // Uses the copy assignment operator of player
 		players.push_back(newPlayer);
-		//cout << "Ive just pushed back the " << i << " Player ! " << newPlayer->toString() << endl;
 	}
 	for(unsigned int i=0; i<otherGame.winners.size();i++){
 				Player* newPlayer = (otherGame.winners)[i]->makePlayer(); // Uses the copy assignment operator of player
@@ -114,7 +103,6 @@ void Game:: clean(){
 
 void Game::init(){
 	for(unsigned int i=0; i<players.size();i++){
-	//	cout << "The " << i << " player is " << players[i]->getName() << endl;
 		for(unsigned int j=0; j<7;j++){
                         Card * c = deck.fetchCard();
 			players[i]->addCard(*c);
@@ -203,10 +191,10 @@ void Game::createPlayer(string input, int playerIndex){
 	playerStrategy=atoi(s.c_str());
 	Player* p;
 	switch(playerStrategy){
-	case 1: /*cout<<"P1 " << playerName << endl*/; p = new PlayerType1(playerName, playerIndex); break;
-	case 2: /*cout<<"P2 " << playerName << endl*/; p = new PlayerType2(playerName, playerIndex); break;
-	case 3: /*cout<<"P3 " << playerName << endl*/; p = new PlayerType3(playerName, playerIndex, 0); break;
-	case 4: /*cout<<"P4 " << playerName << endl;*/ p = new PlayerType4(playerName, playerIndex, 0); break;
+	case 1: p = new PlayerType1(playerName, playerIndex); break;
+	case 2: p = new PlayerType2(playerName, playerIndex); break;
+	case 3: p = new PlayerType3(playerName, playerIndex, 0); break;
+	case 4: p = new PlayerType4(playerName, playerIndex, 0); break;
 	}
 	players.push_back(p);
 }
@@ -218,15 +206,6 @@ void Game::printState(){
 		cout << players[i]->toString() << endl;
 	}
 }
-//
-//bool Game::checkForWinner(){
-//	bool ans=false;
-//	for(unsigned int i=0; i<players.size();i++){
-//		if (players[i]->isWinner())
-//			return true;
-//	}
-//	return ans;
-//}
 
 Deck& Game::getGameDeck(){
 	return deck;
@@ -247,33 +226,9 @@ void Game::printWinner(){
 	if (winners.size()==1){ // Means that there is only 1 winner
 		cout << "***** The winner is: " << winners.front()->getName() << " *****" << endl;
 		cout << "Number of turns: " ; 
-		//cout << "----------" << endl;
-		//cout << "Initial State:" << endl;
-		//cout << initState << endl;
-		//cout << "Final State:" << endl;
-		//cout << "Deck: " << deck.toString() << endl;
-		//for (unsigned int i=0; i<players.size(); i++)
-		//	cout << players[i]->toString();
 	}
 	else if (winners.size()==2){ // Means that there are two winners
 			cout << "***** The winner are: " << winners.front()->getName() << " and " << winners.at(1)->getName() << " *****" << endl;
 			cout << "Number of turns: " << endl;
-			//cout << "----------" << endl;
-			//cout << "Initial State:" << endl;
-			//cout << initState << endl;
-			//cout << "Final State:" << endl;
-			//cout << "Deck: " << deck.toString() << endl;
-			//for (unsigned int i=0; i<players.size(); i++)
-			//	cout << players[i]->toString();
 	}
 }
-
-//
-//int main(int argc, char **argv) {
-//cout << "CheckED" << endl;
-//char* con= argv[1];
-//Game game = Game(con);
-//cout << "CheckED" << endl;
-//
-//return 0;
-//}
